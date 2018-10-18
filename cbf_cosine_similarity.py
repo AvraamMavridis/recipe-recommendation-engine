4870860d5563a107f219d3d2eea264cb14d345e7
AvraamMavridis <avraammauridis@gmail.com>
2018-10-18 09:01:27 +0200

    Feat: Use all attributes to calculate similarity

--- cbf_cosine_similarity.py
+++ cbf_cosine_similarity.py
@@ -8,7 +8,7 @@ import plot_results as plt
 import sys
 
 recipe_to_look = sys.argv[1]
-cosine_similarity_threshold = 0.2
+cosine_similarity_threshold = 0.1
 
 # Flattens an array
 def flat(arr):
@@ -19,25 +19,22 @@ def flat(arr):
 # values into an array of properties
 #
 def get_recipe_attributes(recipe):
-  # keys = filter(lambda key: key != 'title' and key != 'rating', recipe.keys())
-  # return map(lambda x: recipe[x] or 0, keys)
-  return [
-    recipe['fat'] or 0,
-    recipe['protein'] or 0,
-    recipe['calories'] or 0,
-    recipe['sodium'] or 0
-  ]
+  keys = filter(lambda key: key != 'title' and key != 'rating', recipe.keys())
+  return map(lambda x: recipe[x] or 0, keys)
 
-
-def buildVector(iterable1, iterable2):
-    iterable1.sort()
-    iterable2.sort()
-    counter1 = Counter(iterable1)
-    counter2= Counter(iterable2)
-    all_items = set(counter1.keys()).union( set(counter2.keys()) )
-    vector1 = [counter1[k] for k in all_items]
-    vector2 = [counter2[k] for k in all_items]
-    return vector1, vector2
+def normalize_vectors(attrs, attrs2):
+  vector1 = []
+  vector2 = []
+  for idx, val in enumerate(attrs):
+    # We skip the attributes for which both recipes have zero value
+    if attrs2[idx] == 0 and val == 0:
+      continue
+    else:
+      vector1.append(1)
+      vector2.append(0)
+      if(val == attrs2[idx]):
+        vector2[-1] = 1
+  return vector1, vector2
 
 
 def map_attrs_to_ints(recipe, dict):
@@ -46,22 +43,22 @@ def map_attrs_to_ints(recipe, dict):
 
 with open('epi_r.csv') as tsvfile:
   reader = csv.DictReader(tsvfile, delimiter=',')
-  filtered = filter(lambda p: p, reader)[0:200]
-  recipe_to_look = filter(lambda p: p['title'] == recipe_to_look, filtered)[0]
+  filtered = filter(lambda p: p, reader)
+  recipe_to_look = filter(lambda p: recipe_to_look in p['title'], filtered)[0]
 
   attrs = map(lambda x: float(x), get_recipe_attributes(recipe_to_look))
 
   similar_recipes = []
   for rec2 in filtered:
     attrs2 = map(lambda x: float(x), get_recipe_attributes(rec2))
-    [attrs, attrs2] = buildVector(attrs, attrs2)
-    print(attrs, attrs2)
-    cosine_similarity = 1 - spatial.distance.cosine(attrs, attrs2)
+    vc1, vc2 = normalize_vectors(attrs, attrs2)
+    cosine_similarity = 1 - spatial.distance.cosine(vc1, vc2)
 
     if(cosine_similarity > cosine_similarity_threshold and recipe_to_look['title'] != rec2['title']):
       rec2['cosine_similarity'] = cosine_similarity
+      # print(rec2['cosine_similarity'], rec2['title'], attrs, attrs2, vc1, vc2)
       similar_recipes.append(rec2)
 
-  similar_recipes = sorted(similar_recipes, key=lambda x: x['cosine_similarity'])
+  similar_recipes = sorted(similar_recipes, key=lambda x: x['cosine_similarity'])[:100]
 
   plt.draw(similar_recipes, 'cosine_similarity', recipe_to_look['title'])
--- plot_results.py
+++ plot_results.py
@@ -6,12 +6,13 @@ def draw(recipes, similarity_metric, title):
     names = list(map(lambda x: helpers.remove_non_ascii(x['title'][0:20]), recipes))
     similarities = list(map(lambda x: x[similarity_metric], recipes))
 
-    # this is for plotting purpose
     index = np.arange(len(names))
+    plt.rcParams['figure.figsize'] = (15,6)
     plt.bar(index, similarities)
     plt.xlabel('Similar Recipes', fontsize=15)
     plt.ylabel('Similarity', fontsize=15)
+    plt.ylim(0, max(similarities) + 0.1)
     plt.xticks(index, names, fontsize=7, rotation=90)
     plt.title(title)
-    plt.subplots_adjust(bottom=0.4)
+    plt.subplots_adjust(bottom=0.28, left=0.1, right=0.98)
     plt.show()
